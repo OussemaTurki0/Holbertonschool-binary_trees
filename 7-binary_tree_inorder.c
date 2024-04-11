79d6a8df0d74a6a7df1a1ad1f78cf3992e04faa4
oussematurki0 <oussematurki0@gmail.com>
2024-04-11 12:05:50 +0200

    commit

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - Performs in-order traversal on a binary tree
+ * @tree: Pointer to the root node of the tree to traverse
+ * @func: Pointer to a function to call for each node
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (tree == NULL || func == NULL)
+        return;
+
+   
